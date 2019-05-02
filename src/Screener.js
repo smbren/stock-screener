25a8e00684e62921d8978fbc928c5a93fc474d4a
Scott Bren <smbren@ncsu.edu>
2019-05-02 17:39:59 -0400

    working on stock search functionality

--- src/Screener.js
+++ src/Screener.js
@@ -4,14 +4,16 @@ import axios from 'axios';
 class Screener extends Component {
   constructor(props) {
     super(props);
-      this.state = {};   
+      this.state = {
+        ticker: "",
+      };   
   }
 
   componentDidMount() {
 
     const id = "TSLA";
 
-    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${id}&apikey=${process.env.AV_API_TOKEN}`)
+    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${id}&apikey=DEMO`)
       .then(res => {
         console.log(res.data["Global Quote"]);
         this.setState(
@@ -21,10 +23,36 @@ class Screener extends Component {
       .catch(err => console.log(err));
   }
 
+  getStock = (event, ticker) => {
+    event.preventDefault();
+
+    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${process.env.AV_API_KEY}`)
+    .then(res => {
+      console.log(res.data["Global Quote"]);
+      this.setState(
+        res.data["Global Quote"]
+      )
+    })
+    .catch(err => console.log(err));
+
+  }
+
+  handleInput = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     console.log("stock", this.state)
     return (
       <div>
+
+        <form onSubmit={this.getStock(this.state.ticker)}>
+          <input type="search" name="ticker" onChange={this.handleInput}></input>
+          <button type="submit">Search</button>
+        </form>
 
         <p>Symbol: {this.state["01. symbol"]}</p>
         <p>Price: {this.state["05. price"]}</p>
