5c243dae0f1e2b14ed5a17c1ad46cf89193ff54c
Scott Bren <smbren@ncsu.edu>
2019-05-07 18:15:10 -0400

    added navbar

--- src/Screener.js
+++ src/Screener.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import FormControl from 'react-bootstrap/FormControl';
 
 class Screener extends Component {
   constructor(props) {
@@ -33,18 +36,28 @@ class Screener extends Component {
   }
 
   render() {
-    console.log("stock", this.state)
+    console.log("stock", this.state);
+    console.log("change", this.state["9. change"]);
     return (
       <div>
 
-        <Form onSubmit={(e) => this.getStock(e, this.state.ticker)}>
-          <Form.Control type="search" name="ticker" onChange={this.handleInput} />
-          <Button as="input" type="submit" value="Submit" />
-        </Form>
+        <Navbar fixed="top" bg="primary" variant="dark">
+
+          <Navbar.Brand href="#home">Screener</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+
+          <Form inline onSubmit={(e) => this.getStock(e, this.state.ticker)}>
+            <FormControl type="search" name="ticker" onChange={this.handleInput} />
+            <Button variant="outline-light" type="submit">Search</Button>
+          </Form>
+          
+        </Navbar>
 
         <p>Symbol: {this.state["01. symbol"]}</p>
         <p>Price: {this.state["05. price"]}</p>
-        <p>Change: {this.state["10. change percent"]}</p>
+        <p>Change:</p> <p style={{color: this.state["9. change"] > 0 ? 'green' : 'red'}}>{this.state["10. change percent"]}</p>
 
       </div>
     )
